0bc975a00aed1606e94c486594554114f817e21f
Paul Warren <paul_c_warren@yahoo.com>
2016-07-30 23:31:47 -0700

    Move localbroker manifest generation after diego deploy - so we can ru the cell-ip script [#127286279](https://www.pivotaltracker.com/story/show/127286279)

--- scripts/deploy-bosh-lite.sh
+++ scripts/deploy-bosh-lite.sh
@@ -10,12 +10,12 @@ pushd ~/workspace/diego-release
     USE_VOLDRIVER=true ./scripts/generate-bosh-lite-manifests
 popd
 
-pushd ~/workspace/local-volume-release
-    ./scripts/generate-bosh-lite-manifest.sh
-popd
-
 bosh -n -d ~/workspace/cf-release/bosh-lite/deployments/cf.yml deploy
 
 bosh -n -d ~/workspace/diego-release/bosh-lite/deployments/diego.yml deploy
+
+pushd ~/workspace/local-volume-release
+    ./scripts/generate-bosh-lite-manifest.sh
+popd
 
 bosh -n -d ~/workspace/local-volume-release/manifests/bosh-lite/broker.yml deploy
