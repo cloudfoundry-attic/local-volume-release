dadcc9a53bc49f1aef9448d379fc7637d6ab7fbe
Luke Woydziak <luke.woydziak@emc.com>
2016-08-02 09:29:58 -0700

    local broker replaces CephFS in CI - move cell ip generation up to ci specific scripts (#121630277)[https://www.pivotaltracker.com/story/show/121630277]
    Signed-off-by: Kalai Wei <kawei@pivotal.io>

--- templates/generate_manifest.sh
+++ templates/generate_manifest.sh
@@ -3,35 +3,8 @@
 
 
 
-# this is a hack to create a backchannel from the broker to the cell.
-# this is why this is not a production release/broker pair.
-# do not backchannel to real, shared, and/or dfs volume-drivers.
-get_cell_ip ()
-{
-# get the full diego deployment, it should have IPs in it
-taskID=$(curl -I -s -k "https://${6}:${7}@${5}/deployments/cf-warden-diego/vms?format=full" | grep tasks | awk -F "/" '{print $NF}' | tr -d "\r")
-# but it redirects to the wrong URL, so fix that and get the task
-taskIP=$(curl I -s -k "https://${6}:${7}@${5}/tasks/${taskID}")
-# but that's got an asynchronous result endpoint to poll, so wait
-sleep 5
-# finally get the task result and rip the cell IP out of its json
-cellIP=$(curl -s -k "https://${6}:${7}@${5}/tasks/${taskID}/output?type=result" | jq -r 'select(.job_name | contains("cell_z1")) | .ips[]')
-}
-
-print_cell_ip_stub ()
-{
-get_cell_ip
-cat > ${PWD}/cell-ip.yml << EOF
----
-properties:
-  localbroker:
-    localdriver-url: http://${cellIP}:9089
-EOF
-}
-
-
 usage () {
-    echo "Usage: generate_manifest.sh bosh-lite|aws cf-manifest director-stub localbroker-creds-stub bosh_target bosh_username bosh_password"
+    echo "Usage: generate_manifest.sh bosh-lite|aws cf-manifest director-stub localbroker-creds-stub cell-ip-stub"
     echo " * default"
     exit 1
 }
@@ -51,15 +24,13 @@ fi
 
 if [ "$1" == "aws" ]
   then
-    print_cell_ip_stub
-
     MANIFEST_NAME=localvolume-aws-manifest
 
     spiff merge ${templates}/localvolume-manifest-aws.yml \
     $2 \
     $3 \
     $4 \
-    ${PWD}/cell-ip.yml \
+    $5 \
     ${templates}/stubs/toplevel-manifest-overrides.yml \
     > $PWD/$MANIFEST_NAME.yml
 fi
