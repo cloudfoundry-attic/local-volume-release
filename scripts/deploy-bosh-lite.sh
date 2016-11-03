08fbdfba534e275de30bd0d2ba1b915db3d76407
Luke Woydziak <luke.woydziak@emc.com>
2016-11-03 15:13:33 -0700

    update bosh lite scripts to use bosh addons for localdriver deployment instead of diego manifest generation [#120977577](https://www.pivotaltracker.com/story/show/120977577)
    Signed-off-by: Julian Hjortshoj <julian.hjortshoj@emc.com>

--- scripts/deploy-bosh-lite.sh
+++ scripts/deploy-bosh-lite.sh
@@ -8,15 +8,16 @@ pushd ~/workspace/cf-release
 popd
 
 pushd ~/workspace/diego-release
-    USE_VOLDRIVER=true ./scripts/generate-bosh-lite-manifests
+    ./scripts/generate-bosh-lite-manifests
+popd
+
+pushd ~/workspace/local-volume-release
+    ./scripts/generate-bosh-lite-manifest.sh $(bosh status --uuid) https://192.168.50.4:25555 admin admin admin admin
+    bosh -n update runtime-config ./templates/localdriver-addon.yml
 popd
 
 bosh -n -d ~/workspace/cf-release/bosh-lite/deployments/cf.yml deploy
 
 bosh -n -d ~/workspace/diego-release/bosh-lite/deployments/diego.yml deploy
-
-pushd ~/workspace/local-volume-release
-    ./scripts/generate-bosh-lite-manifest.sh $(bosh status --uuid) https://192.168.50.4:25555 admin admin admin admin
-popd
 
 bosh -n -d ~/workspace/local-volume-release/localvolume-boshlite-manifest.yml deploy
