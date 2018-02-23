19830af6c3db39b33992befe90ed97d5fd538e8e
Paul Warren <paul.warren@emc.com>
2018-02-23 12:15:27 -0800

    add windows unit test tasks and scripts
    [##155268532](https://www.pivotaltracker.com/story/show/#155268532)
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

---
+++ scripts/ci/run_driver_unit.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = “Stop”;
+trap { $host.SetShouldExit(1) }
+
+cd local-volume-release
+
+
+$env:GOPATH=$PWD
+$env:PATH="$PWD/bin;$env:PATH"
+
+go install github.com/onsi/ginkgo/ginkgo
+
+cd src/code.cloudfoundry.org/localdriver
+ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
+
