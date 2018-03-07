216d985231e63f67716cf1f90d3679d76f4c2fa5
Paul Warren <paul.warren@emc.com>
2018-03-07 14:21:06 -0800

    add driver cert test for windows
    [#155666972](https://www.pivotaltracker.com/story/show/155666972)
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

---
+++ scripts/ci/run_driver_cert.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = “Stop”;
+trap { $host.SetShouldExit(1) }
+
+cd local-volume-release
+
+$env:GOPATH=$PWD
+$env:PATH="$PWD/bin;$env:PATH"
+
+go install github.com/onsi/ginkgo/ginkgo
+
+go build -o "./localdriver" "src/code.cloudfoundry.org/localdriver/cmd/localdriver/main.go"
+
+go get -t code.cloudfoundry.org/volume_driver_cert
+
+$env:FIXTURE_FILENAME=$PWD/scripts/fixtures/certification_tcp.json
+
+mkdir voldriver_plugins
+mkdir mountdir
+Start-Process -NoNewWindow ./localdriver -listenAddr="0.0.0.0:9776" -transport="tcp" -mountDir="./mountdir" -driversPath="./voldriver_plugins"
+
+cd src/code.cloudfoundry.org/volume_driver_cert
+ginkgo
+
+Stop-Process -Name "localdriver"
