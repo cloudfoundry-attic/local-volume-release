5d78589c656de0d4ae9bbea5612d656b238e69b3
Kai Xiang <kxiang@pivotal.io>
2018-03-19 14:25:12 -0700

    add CheckLastExitCode so pipeline will be red if ginkgo fails
    [#155668387](https://www.pivotaltracker.com/story/show/155668387)

--- scripts/ci/run_driver_unit.ps1
+++ scripts/ci/run_driver_unit.ps1
@@ -3,6 +3,21 @@ trap { $host.SetShouldExit(1) }
 
 cd local-volume-release
 
+function CheckLastExitCode {
+    param ([int[]]$SuccessCodes = @(0), [scriptblock]$CleanupScript=$null)
+
+    if ($SuccessCodes -notcontains $LastExitCode) {
+        if ($CleanupScript) {
+            "Executing cleanup script: $CleanupScript"
+            &$CleanupScript
+        }
+        $msg = @"
+EXE RETURNED EXIT CODE $LastExitCode
+CALLSTACK:$(Get-PSCallStack | Out-String)
+"@
+        throw $msg
+    }
+}
 
 $env:GOPATH=$PWD
 $env:PATH="$PWD/bin;$env:PATH"
@@ -12,3 +27,4 @@ go install github.com/onsi/ginkgo/ginkgo
 cd src/code.cloudfoundry.org/localdriver
 ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
 
+CheckLastExitCode
